f1ffa0227e497dfafeeee539779a0e25e1893366
Vitor <fernandofernandesvitor@gmail.com>
2022-06-19 00:01:25 -0400

    using cypress grep

--- cypress/integration/authenticatedScenarios.spec.js
+++ cypress/integration/authenticatedScenarios.spec.js
@@ -32,7 +32,7 @@ describe('Scenarios where authentication is a pre-requirement', () => {
     })
   })
 
-  it.only('logs out', () => {
+  it.only('logs out', { tags: '@desktop-and-tablet' }, () => {
     cy.visit('/')
     cy.wait('@getNotes')
 
