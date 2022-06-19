2680bad5ee3dc0c576a6b5ee56da3a80d6044b66
Vitor <fernandofernandesvitor@gmail.com>
2022-06-19 00:24:20 -0400

    tests failing

--- cypress/integration/authenticatedScenarios.spec.js
+++ cypress/integration/authenticatedScenarios.spec.js
@@ -34,13 +34,8 @@ describe('Scenarios where authentication is a pre-requirement', () => {
 
   it('logs out', { tags: '@desktop-and-tablet' }, () => {
     cy.visit('/')
-    cy.wait('@getNotes')
+    cy.wait(2000)
 
-    if (Cypress.config('viewportWidth') < Cypress.env('viewportWidthBreakpoint')) {
-      cy.get('.navbar-toggle.collapsed')
-        .should('be.visible')
-        .click()
-    }
     /* ==== Generated with Cypress Studio ==== */
     cy.get('.nav > :nth-child(2) > a').click()
     cy.get('#email').should('be.visible')
